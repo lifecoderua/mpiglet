81f5e4845747a486c458908c323ee17a5fa2dc77
lifecoder <lifecoder.ua@gmail.com>
2019-06-17 13:44:23 +0300

    add extra string reader spec

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,7 +2,7 @@ import Utils from "../src/utils/utils";
 
 describe('Utils', () => {
   describe('#uintToString', () => {
-    it('should convert simple characters', () => {
+    test('should convert simple characters', () => {
       const input = new Uint8Array(4);
       input[0] = 0x41;
       input[1] = 0x68;
@@ -12,13 +12,24 @@ describe('Utils', () => {
       expect(Utils.uintToString(input)).toEqual('Ahem');
     });
 
-    it('should convert multibyte characters', () => {
+    test('should convert multibyte characters', () => {
       const input = new Uint8Array(3);
       input[0] = 0xe2;
       input[1] = 0x82;
       input[2] = 0xac;
 
       expect(Utils.uintToString(input)).toEqual('€');
+    });
+
+    test('should read combined strings', () => {
+      const input = new Uint8Array(5);
+      input[0] = 0x65;
+      input[1] = 0xe2;
+      input[2] = 0x82;
+      input[3] = 0xac;
+      input[4] = 0x6D;
+
+      expect(Utils.uintToString(input)).toEqual('e€m');
     })
   });
 });
